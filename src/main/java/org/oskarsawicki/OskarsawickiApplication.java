9a55353d618d5f7c4eba4dfbc83d41f8832dc880
SzS19 <szymonstach03@gmail.com>
2025-03-09 21:32:18 +0100

    first commit

---
+++ src/main/java/org/oskarsawicki/OskarsawickiApplication.java
@@ -0,0 +1,13 @@
+package org.oskarsawicki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OskarsawickiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OskarsawickiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/oskarsawicki/OskarsawickiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.oskarsawicki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OskarsawickiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
