388be3dcd39ba946a2536622af1ea09cd451c8ca
SzS19 <szymonstach03@gmail.com>
2025-03-14 21:47:51 +0100

    bigger video on mobile device

--- src/main/java/org/oskarsawicki/config/SpringSecurity.java
+++ src/main/java/org/oskarsawicki/config/SpringSecurity.java
@@ -14,7 +14,7 @@ public class SpringSecurity {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/css/**", "/js/**", "/images/**", "/info").permitAll()
+                        .requestMatchers("/", "/css/**", "/js/**", "/images/**", "/info", "#black-section").permitAll()
                         .anyRequest().authenticated()
                 );
 
